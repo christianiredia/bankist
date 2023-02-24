ddf70553a1cd25bc0b73438e2788a82d5da9eba0
Christian Iredia <101851819+christianiredia@users.noreply.github.com>
2023-02-24 17:04:30 -0600

    When one of the nav links are clicked on it will scroll down to that specific section

--- index.js
+++ index.js
@@ -30,9 +30,19 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
+// button scroll to
 const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
 
 btnScrollTo.addEventListener("click", function (e) {
   section1.scrollIntoView({ behavior: "smooth" });
+});
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
 });
