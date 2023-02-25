9db5ee97246e4d02c2fcc1b029adfc706f6e0a28
Christian Iredia <101851819+christianiredia@users.noreply.github.com>
2023-02-24 22:11:29 -0600

    refactored the scroll feature for the stick nav container

--- index.js
+++ index.js
@@ -94,12 +94,17 @@ nav.addEventListener("mouseout", (e) => {
   handleHover(e, 1);
 });
 
-// sticky naviataion
-const initialCoords = section1.getBoundingClientRect();
-window.addEventListener("scroll", (e) => {
-  if (window.scrollY > initialCoords.top) {
-    nav.classList.add("sticky");
-  } else {
-    nav.classList.remove("sticky");
-  }
+const header = document.querySelector(".header");
+const navHeight = nav.getBoundingClientRect().height;
+const stickyNav = (entries) => {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
 });
+headerObserver.observe(header);
