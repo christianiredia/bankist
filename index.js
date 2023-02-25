a6eb5668b7c9e24e788f450bbef898b024f3d837
Christian Iredia <101851819+christianiredia@users.noreply.github.com>
2023-02-24 21:29:20 -0600

    When nav links are hovered over the opacity is changed on all other links

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const nav = document.querySelector(".nav");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
 
 const openModal = function (e) {
   e.preventDefault();
@@ -31,8 +37,6 @@ document.addEventListener("keydown", function (e) {
 });
 
 // button scroll to
-const btnScrollTo = document.querySelector(".btn--scroll-to");
-const section1 = document.querySelector("#section--1");
 
 btnScrollTo.addEventListener("click", function (e) {
   section1.scrollIntoView({ behavior: "smooth" });
@@ -48,9 +52,6 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
 });
 
 // Tab components
-const tabs = document.querySelectorAll(".operations__tab");
-const tabsContainer = document.querySelector(".operations__tab-container");
-const tabsContent = document.querySelectorAll(".operations__content");
 
 tabsContainer.addEventListener("click", (e) => {
   const clicked = e.target.closest(".operations__tab");
@@ -67,4 +68,28 @@ tabsContainer.addEventListener("click", (e) => {
   document
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add("operations__content--active");
+});
+
+// menu fade animation
+const handleHover = (e, opacity) => {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    siblings.forEach((el) => {
+      if (el !== link) {
+        el.style.opacity = opacity;
+      }
+      logo.style.opacity = opacity;
+    });
+  }
+};
+
+nav.addEventListener("mouseover", (e) => {
+  handleHover(e, 0.5);
+});
+
+nav.addEventListener("mouseout", (e) => {
+  handleHover(e, 1);
 });
