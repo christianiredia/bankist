ddc8fe864bcb17af2be17d61ecc28440cf333341
Christian Iredia <101851819+christianiredia@users.noreply.github.com>
2023-02-24 20:40:46 -0600

    Add and remove and active class based off the tab that is clicked

--- index.js
+++ index.js
@@ -46,3 +46,25 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
+
+// Tab components
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+tabsContainer.addEventListener("click", (e) => {
+  const clicked = e.target.closest(".operations__tab");
+  if (!clicked) return;
+
+  //remove active class
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+  tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
+
+  //activate tab
+  clicked.classList.add("operations__tab--active");
+
+  //activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
